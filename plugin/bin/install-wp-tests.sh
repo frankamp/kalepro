a7df841bd2bc85cedc150be7d0dfaa2e585b4281
frankamp <frankamp@gmail.com>
2017-04-10 00:15:27 -0700

    Trying to install a plugin at travis

--- plugin/bin/install-wp-tests.sh
+++ plugin/bin/install-wp-tests.sh
@@ -68,7 +68,8 @@ install_wp() {
 		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
 		tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 	fi
-
+	download https://downloads.wordpress.org/plugin/wordpress-importer.0.6.3.zip  /tmp/wordpress-importer.0.6.3.zip
+	unzip -q /tmp/wordpress-importer.0.6.3.zip -d $WP_CORE_DIR/plugins/wordpress-importer/
 	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
 }
 
