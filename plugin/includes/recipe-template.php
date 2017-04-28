58b350a81bef3373cb7a8ea23b07e0fe5718fc45
jshltz <john@minimalistbaker.com>
2017-04-28 16:20:35 -0700

    Restructuring recipe data to match template

--- plugin/includes/recipe-template.php
+++ plugin/includes/recipe-template.php
@@ -1,20 +1,18 @@
 <script type="application/ld+json"><?=$viewhelper::ldjson($recipe)?></script>
 <div class="recipe-pro-recipe rp" itemscope="" itemtype="http://schema.org/Recipe"> 
-	<div class="rp-ratings" itemprop="aggregateRating" itemscope="" itemtype="http://schema.org/AggregateRating"> 
-		<div>
-			<div><span ><span itemprop="ratingValue"><?= number_format($recipe->ratingValue, 1) ?></span> from <span itemprop="ratingCount"><?= $recipe->ratingCount ?></span> reviews</span>
+	<div class="rp-name" itemprop="name"><?= $recipe->title ?></div> 
+	<div class="recipe-pro-print"><button class="rp-print"><?= $labels['print'] ?></button></div>
+	<div class="rp-topright">
+		<div class="rp-previewimage"> 
+			<img itemprop="image" src="<?= $recipe->imageUrl ?>" width="205"> 
+		</div>
+		<div class="rp-ratings" itemprop="aggregateRating" itemscope="" itemtype="http://schema.org/AggregateRating"> 
+			<div>
+				<div><span ><span itemprop="ratingValue"><?= number_format($recipe->ratingValue, 1) ?></span> from <span itemprop="ratingCount"><?= $recipe->ratingCount ?></span> reviews</span>
+				</div> 
 			</div> 
 		</div> 
-	</div> 
-	<div class="rp-name" itemprop="name"><?= $recipe->title ?></div> 
-	<div class="rp-topright"><div class="rp-previewimage"> 
-		<img itemprop="image" src="<?= $recipe->imageUrl ?>" width="205"> 
-	</div> 
-	<div class="recipe-pro-print"><button class="rp-print"><?= $labels['print'] ?></button></div></div>
-	<div class="rp-overview">
-	<div class="rp-recipetype"><?= $labels['recipe_type'] ?>: <span itemprop="recipeCategory"><?= $recipe->type ?></span></div>
-	<div class="rp-cuisine"><?= $labels['cuisine'] ?>: <span itemprop="recipeCuisine"><?= $recipe->cuisine ?></span></div>
-	<div class="rp-author"><?= $labels['author'] ?>: <span itemprop="author"><?= $recipe->author ?></span></div>
+	</div>
 	<div>
 		<div class="rp-preptime">
 			<div><?= $labels['prep_time'] ?></div>
@@ -29,8 +27,10 @@
 			<div> <time itemprop="totalTime" datetime="<?= $viewhelper::interval( $recipe->totalTime() ) ?>"><?= $viewhelper::prettyInterval( $recipe->totalTime() ) ?></time> </div>
 		</div>
 	</div>
+	<div class="rp-cuisine"><?= $labels['cuisine'] ?>: <span itemprop="recipeCuisine"><?= $recipe->cuisine ?></span></div>
 	<div class="rp-serving">Serves: <span itemprop="recipeYield"><?= $recipe->yield ?></span></div>
-	<div class="rp-description"><span itemprop="description"><?= $recipe->description ?></span></div>
+	<div class="rp-overview">
+		<div class="rp-description"><span itemprop="description"><?= $recipe->description ?></span></div>
 	</div>
 	<div> 
 		<div class="rp-ingredientstitle"><?= $labels['ingredients'] ?></div> 
@@ -76,4 +76,6 @@
 		</div> 
 		<div></div> 
 	</div>
+	<div class="rp-author"><?= $labels['author'] ?>: <span itemprop="author"><?= $recipe->author ?></span></div>
+	<div class="rp-recipetype"><?= $labels['recipe_type'] ?>: <span itemprop="recipeCategory"><?= $recipe->type ?></span></div>	
 </div>
