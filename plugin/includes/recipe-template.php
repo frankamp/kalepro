bd37ea3605b5a905827b49004c8851c9d5368c27
jshltz <john@minimalistbaker.com>
2017-04-29 15:27:26 -0700

    Revert "Updating author and recipe type CSS"
    This reverts commit c5817e2631b14cbf034241a6771662f55b9a3dbf.

--- plugin/includes/recipe-template.php
+++ plugin/includes/recipe-template.php
@@ -80,8 +80,6 @@
 		</div> 
 		<div></div> 
 	</div>
-	<div class="rp-detailsafter">
-		<span class="rp-author"><?= $labels['author'] ?>: <span itemprop="author"><?= $recipe->author ?></span></span>
-		<span class="rp-recipetype"><?= $labels['recipe_type'] ?>: <span itemprop="recipeCategory"><?= $recipe->type ?></span></span>
-	</div>
+	<div class="rp-author"><?= $labels['author'] ?>: <span itemprop="author"><?= $recipe->author ?></span></div>
+	<div class="rp-recipetype"><?= $labels['recipe_type'] ?>: <span itemprop="recipeCategory"><?= $recipe->type ?></span></div>
 </div>
