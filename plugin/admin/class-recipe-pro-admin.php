3aef8e2a7e22a9e38f1b0707eb11bbb7d0c210de
jshltz <john@minimalistbaker.com>
2017-04-30 10:57:35 -0700

    Trying to move admin image size to css

--- plugin/admin/class-recipe-pro-admin.php
+++ plugin/admin/class-recipe-pro-admin.php
@@ -281,7 +281,7 @@ class Recipe_Pro_Admin {
 					<div class="right">
 						<p>
 						<div class='image-preview-wrapper'>
-							<img id='image-preview' src='<%= _.escape(imageUrl) %>' width='150' height='150' style=' width: 200px; height: auto;'>
+							<img id='image-preview' class='rp-image-admin' src='<%= _.escape(imageUrl) %>'>
 						</div>
 						<input id="upload_image_button" type="button" class="button rp-image-select" value="<?php _e( 'Select Recipe Image', 'recipe-pro' ); ?>" />
 						<input type='hidden' name='recipepro_imageurl' id='imageUrl' value='<%= _.escape(imageUrl) %>' />
