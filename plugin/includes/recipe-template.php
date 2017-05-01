19130953df1f116c7dbaf66e7894dce4fad89564
jshltz <john@minimalistbaker.com>
2017-05-01 14:31:36 -0700

    CSS Updates and Mobile Additions

--- plugin/includes/recipe-template.php
+++ plugin/includes/recipe-template.php
@@ -18,19 +18,19 @@
 	<div class="rp-times">
 <?php if ( !$viewhelper::intervalsAreEqual( $recipe->prepTime, new DateInterval("PT0M") ) ): ?>
 	<div class="rp-preptime">
-		<div><?= $labels['prep_time'] ?></div>
+		<div class="rp-timetitle"><?= $labels['prep_time'] ?></div>
 		<div class="rp-time"><time itemprop="prepTime" datetime="<?= $viewhelper::interval( $recipe->prepTime ) ?>"><?= $viewhelper::prettyInterval( $recipe->prepTime ) ?></time> </div>
 	</div>
 <?php endif; ?>
 <?php if ( !$viewhelper::intervalsAreEqual( $recipe->cookTime, new DateInterval("PT0M") ) ): ?>
 	<div class="rp-cooktime">
-		<div><?= $labels['cook_time'] ?></div>
+		<div class="rp-timetitle"><?= $labels['cook_time'] ?></div>
 		<div class="rp-time"><time itemprop="cookTime" datetime="<?=  $viewhelper::interval( $recipe->cookTime ) ?>"><?= $viewhelper::prettyInterval( $recipe->cookTime ) ?></time> </div>
 	</div>
 <?php endif; ?>
 <?php if ( !$viewhelper::intervalsAreEqual( $recipe->totalTime(), new DateInterval("PT0M") ) ): ?>
 	<div class="rp-totaltime">
-		<div><?= $labels['total_time'] ?></div>
+		<div class="rp-timetitle"><?= $labels['total_time'] ?></div>
 		<div class="rp-time"><time itemprop="totalTime" datetime="<?= $viewhelper::interval( $recipe->totalTime() ) ?>"><?= $viewhelper::prettyInterval( $recipe->totalTime() ) ?></time> </div>
 	</div>
 <?php endif; ?>
